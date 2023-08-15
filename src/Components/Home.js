5552dfd2e170e5cde270bc664e8a3bab77309de0
Shubhamkitian007 <imshubhamkitan@gmail.com>
2023-08-16 01:02:02 +0530

    update icon

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Home() {
   return (
     <>
-        <h1>Home Page</h1> 
+        <h1>Welcome to Home Page</h1> 
+        <h3>Thanks for your visit</h3>
+        <h3>This portfolio Website is in development Mode</h3>
     </>
   )
 }
